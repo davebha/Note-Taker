21d86868d150d329e9f4c6cc4cc41b09a3a3b694
davebha <davebharat93@gmail.com>
2021-06-12 16:08:02 -0400

    Setup express application along with port to run it on

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require("express");
 
+//setup express
+const app = express();
+const PORT = 8000;
+
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
