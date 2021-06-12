4e5ec7458376143556486e67e8561d06f5788903
davebha <davebharat93@gmail.com>
2021-06-12 16:35:15 -0400

    Only serve html or html extension files from public folder

--- server.js
+++ server.js
@@ -4,5 +4,13 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+const indexUrl = `${__direname}/public`;
+
+app.use(
+  express.static(indexUrl, {
+    extensions: ["html", "htm"]
+  })
+);
+
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
