a18d100def4bd6cfd89198b4800f4ac9e08889b5
Bharat Dave <davebharat93@gmail.com>
2021-06-24 19:59:22 -0400

    Add the link to open the application on client

--- server.js
+++ server.js
@@ -3,18 +3,20 @@ const express = require("express");
 
 //setup express
 const app = express();
-const PORT = 8000;
+const PORT = 8080;
 
 //setup routes
-const apiRoutes = require("./routes/apiRoutes");
+//const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use("/api", apiRoutes);
+//app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 // Start the server on the port
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Listening on PORT:http://localhost:${PORT}`)
+);
