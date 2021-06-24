cef19726d0d8d4ac4ae16d709e287ae3a111372a
Bharat Dave <davebharat93@gmail.com>
2021-06-24 19:13:03 -0400

    Setup routes to html as well as api routes

--- server.js
+++ server.js
@@ -6,21 +6,15 @@ const app = express();
 const PORT = 8000;
 
 //setup routes
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-/*const indexUrl = `${__dirname}/public`;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use(
-  express.static(indexUrl, {
-    extensions: ["html", "htm"]
-  })
-);
-
-app.get("*", (request, response) => {
-  response.redirect("/index.html");
-});
-app.get("/notes", (request, response) => {
-  response.redirect(`/notes.html`);
-});*/
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
