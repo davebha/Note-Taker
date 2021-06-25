31b3134c5b530c8ef83523647617442a8c1eaecb
Bharat Dave <davebharat93@gmail.com>
2021-06-24 20:10:34 -0400

    Implement fetching of notes using GET

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,14 @@
+const router = require("express").Router();
+const store = require("../db/store");
 
+//GET
+router.get("/notes", async (request, response) => {
+  let results = await store.getNotes();
+  response.send(results);
+});
+
+//ADD NEW NOTES
+
+//DELETE NOTE BY ID
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const app = express();
 const PORT = 8080;
 
 //setup routes
-//const apiRoutes = require("./routes/apiRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//app.use("/api", apiRoutes);
+app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 // Start the server on the port
